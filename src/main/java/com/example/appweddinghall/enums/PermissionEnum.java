d1c460facb9a497cf6849b2816f57069fce14ec8
sehrliA02 <farkhodovalisher>
2023-12-09 19:46:01 +0500

    initial commit

--- src/main/java/com/example/appweddinghall/enums/PermissionEnum.java
+++ src/main/java/com/example/appweddinghall/enums/PermissionEnum.java
@@ -2,5 +2,6 @@ package com.example.appweddinghall.enums;
 
 public enum PermissionEnum {
     ADD_USER, GET_USER, EDIT_USER, DELETE_USER,
-    ADD_ROLE, GET_ROLE, EDIT_ROLE, DELETE_ROLE
+    ADD_ROLE, GET_ROLE, EDIT_ROLE, DELETE_ROLE,
+    ADD_PRODUCT
 }
